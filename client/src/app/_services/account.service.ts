dfa48a11dc980d363d7ee0c65b64be275b231a39
Amol Khairnar <amol.khainar@amdocs.com>
2021-05-18 09:05:50 +0530

    Added the  user interface

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ReplaySubject } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import { User } from '../_modals/user';
 
 @Injectable({
@@ -10,7 +11,7 @@ import { User } from '../_modals/user';
 export class AccountService {
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
-  baseUrl = 'https://localhost:5101/api/'
+  baseUrl = environment.apiUrl;
   constructor(private http: HttpClient) { }
 
   login(model: any) {
